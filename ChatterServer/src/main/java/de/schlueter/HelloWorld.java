984c81e1b6e90fd852c6fd10e1c25ccfa3c8fc9d
Maduki-tech <d.schlueter1011@gmail.com>
2024-01-06 12:24:22 +0100

    able to talk to the backend from nextjs

---
+++ ChatterServer/src/main/java/de/schlueter/HelloWorld.java
@@ -0,0 +1,17 @@
+package de.schlueter;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/")
+    ResponseEntity<String> helloWorld() {
+        String json = "{\"message\": \"Hello World!\"}";
+        return new ResponseEntity<>(json, null, 200);
+
+    }
+}
--- clientChatter/prettier.config.js
+++ clientChatter/prettier.config.js
@@ -1,5 +1,9 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  semi: true,
+  singleQuote: false,
+  printWidth: 100,
+  tabWidth: 4,
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
