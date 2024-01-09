b4ccceb42b9cd1318ab30fa67ad5f9c4940b11cf
Maduki-tech <d.schlueter1011@gmail.com>
2024-01-09 17:42:15 +0100

    added docker compose watch file

--- clientChatter/next.config.js
+++ clientChatter/next.config.js
@@ -4,7 +4,8 @@
  */
 await import("./src/env.js");
 
-/** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  output:{standalone:true},
+};
 
 export default config;
--- clientChatter/src/app/page.tsx
+++ clientChatter/src/app/page.tsx
@@ -12,6 +12,8 @@ export default function HomePage() {
         fetcher().catch((err) => console.error(err));
     });
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white"></main>
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+            David
+        </main>
     );
 }
